0dff48e787558a63238304d27c5cda99bcdf0026
Eric Putnam <96210513+eputnam77@users.noreply.github.com>
2025-06-25 11:52:31 -0400

    feat(plugin): add remark-critic assets

--- packages/docusaurus-plugin-trackchanges/src/index.js
+++ packages/docusaurus-plugin-trackchanges/src/index.js
@@ -1,5 +1,32 @@
-module.exports = function () {
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function (context) {
+  const { siteDir } = context;
+  const staticDir = path.join(siteDir, 'static');
+  const assetsDir = __dirname;
+  const assets = ['marktrace.css', 'marktrace.js'];
+
   return {
     name: 'docusaurus-plugin-trackchanges',
+
+    configureMDX(mdxOptions) {
+      const remark = require('remark-critic-markup');
+      mdxOptions.remarkPlugins = [...(mdxOptions.remarkPlugins || []), remark];
+      return mdxOptions;
+    },
+
+    async loadContent() {
+      await fs.promises.mkdir(staticDir, { recursive: true });
+      await Promise.all(
+        assets.map(async (name) => {
+          const src = path.join(assetsDir, name);
+          const dest = path.join(staticDir, name);
+          if (fs.existsSync(src)) {
+            await fs.promises.copyFile(src, dest);
+          }
+        }),
+      );
+    },
   };
 };
